224790d74524e8d5bc7cef16d1c3bac7c0ba6bce
ErkanKabil <58916013+ErkanKabil@users.noreply.github.com>
2024-02-20 18:28:12 +0300

    Update sort.ts

--- src/core/utils/sort.ts
+++ src/core/utils/sort.ts
@@ -1,6 +1,6 @@
 import {IProduct} from "@/core/types/product";
 
-export const sortByParam = (items: IProduct[], value: string) => {
+export const sortByParam = (items: IProduct[], value: string | undefined) => {
     let sorted = items;
     if (value === "BY_PRICE_ASCENDING") {
         sorted = items.sort((a,b) => b.priceO - a.priceO)
