ac0d65aeeeef7616c9655e29502b2bbf934c2bbc
ErkanKabil <58916013+ErkanKabil@users.noreply.github.com>
2024-02-20 18:29:35 +0300

    Update sortTypes.ts

--- src/core/constants/sortTypes.ts
+++ src/core/constants/sortTypes.ts
@@ -2,11 +2,9 @@ export const sortTypes = [
     {
         value: 'BY_PRICE_ASCENDING',
         label: 'Sort by: Price - Low to High',
-        onChange: (items, param) => items.sort((a, b) => a[param] - b[param])
     },
     {
         value: 'BY_PRICE_DESCENDING',
         label: 'Sort by: Price - High to Low',
-        onChange: (items, param) => items.sort((a, b) => b[param] - a[param])
     }
 ]
